fixing some errors with signs in loss functions

examples/cifar/train.py
@@ -1,6 +1,11 @@
+import sys
+sys.path.insert(0, "/home/shlomi/global_objectives_pytorch")
+
+
 import os
 import numpy as np
 import torch
+import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
@@ -43,18 +48,18 @@ test_set = torchvision.datasets.CIFAR10(root='./data', train=False,
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=512,
                                           shuffle=False, num_workers=2)
 
-net = vgg.VGG11(num_classes=10)
+net = vgg.VGG16(num_classes=10)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-criterion = AUCPRLoss(num_classes=10)
-
+#criterion = nn.CrossEntropyLoss()
+criterion = AUCPRLoss(num_classes=10, num_anchors=30)
 
 params = list(net.parameters()) + list(criterion.parameters())
 
-optimizer_net = optim.SGD(params, lr=1e-2)
-scheduler_net = optim.lr_scheduler.StepLR(optimizer_net, step_size=5, gamma=0.5)
+optimizer_net = optim.SGD(params, lr=1e-1)
+scheduler_net = optim.lr_scheduler.StepLR(optimizer_net, step_size=15, gamma=0.5)
 
 
 net = net.to(device)

examples/toy_examples/trainer.py
@@ -16,8 +16,8 @@ def train_model(data, use_global_objectives,
 
     w = torch.tensor([1.0, 1.0],  requires_grad=True, device=device)
     b = torch.tensor([0.0], requires_grad=True, device=device)
-    x = torch.tensor(data['train_data'], requires_grad=False, device=device).float()
-    labels = torch.tensor(data['train_labels'], requires_grad=False, device=device).float()
+    x = torch.tensor(data['train_data'], requires_grad=False, device=device).float().cuda()
+    labels = torch.tensor(data['train_labels'], requires_grad=False, device=device).float().cuda()
 
     params = [w, b]
     if not use_global_objectives:
@@ -25,7 +25,7 @@ def train_model(data, use_global_objectives,
     else:
         params += list(criterion.parameters())
 
-    optimizer = optim.SGD(params, lr=lr)
+    optimizer = optim.Adam(params, lr=lr)
     checkpoint_step = train_iteration // num_checkpoints
 
     for t in range(train_iteration):
@@ -48,7 +48,7 @@ def train_model(data, use_global_objectives,
                 at_target_rate
             )
 
-            print('Loss = {}, {} = {:.2f} @ {} = {:.2f}'.format(
+            print('Loss = {}, {} = {:.3f} @ {} = {:.3f}'.format(
                 loss.data, obj_type, obj_rate,
                 at_target_type, target_rate))
 

global_objectives/base.py
@@ -96,7 +96,7 @@ class BaseLoss(nn.Module):
                 negative_weights=negative_weights,
             )
 
-            per_anchor_loss = weights.unsqueeze(-1) * hinge_loss + lambda_term
+            per_anchor_loss = weights.unsqueeze(-1) * hinge_loss - lambda_term
             # loss = per_label_loss
             loss = per_anchor_loss.sum(2) * self.delta
             loss /= (self.target_range[1] - self.target_range[0] - self.delta)
@@ -109,7 +109,7 @@ class BaseLoss(nn.Module):
                 positive_weights=positive_weights,
                 negative_weights=negative_weights,
             )
-            loss = weights * hinge_loss + lambda_term
+            loss = weights * hinge_loss - lambda_term
 
         if not reduce:
             return loss